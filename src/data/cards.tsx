ad23bb73b87228fcb5f2dfe53f71f4ac5ea3fede
Gabriel Sanchez <gisgarci99@gmail.com>
2026-05-04 16:46:19 -0600

    Aaaaa

---
+++ src/data/cards.tsx
@@ -0,0 +1,104 @@
+const cards = [
+    {
+        number: 1,
+        type: "hollographic",
+        content: "logo"
+    },
+    {
+        number: 2,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 3,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 4,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 5,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 6,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 7,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 8,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 9,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 10,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 11,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 12,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 13,
+        type: "horizontal",
+        content: "lineup"
+    },
+    {
+        number: 14,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 15,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 16,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 17,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 18,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 19,
+        type: "normal",
+        content: "player"
+    },
+    {
+        number: 20,
+        type: "normal",
+        content: "player"
+    },
+]
+
+export default cards;
---
+++ src/data/countries.tsx
@@ -0,0 +1,14 @@
+const countries = [
+    {
+        "name": "México",
+        "abrv": "MEX",
+        "colors": ["#227823","#F2F2F2","#A60000"]
+    },
+    {
+        "name": "España",
+        "abrv": "ESP",
+        "colors": ["#BD0000","#FFDA0F","#BD0000"]
+    }
+]
+
+export default countries;
